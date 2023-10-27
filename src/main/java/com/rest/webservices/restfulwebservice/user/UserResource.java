7fc66ed8311a9c1e622a9bb94a4b80b0a1e8e245
김산해 <sanhae1996@gmail.com>
2023-10-27 20:32:36 +0900

    Implementing Exception Handling - 404 Resource Not Found

--- src/main/java/com/rest/webservices/restfulwebservice/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservice/user/UserResource.java
@@ -27,7 +27,11 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id)
     {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null) throw new UserNotFoundException("id:"+id);
+
+        return user;
     }
 
     //POST /users
