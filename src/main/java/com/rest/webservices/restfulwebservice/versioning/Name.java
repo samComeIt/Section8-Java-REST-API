09a0a0acd529b8e86e93b8a5dd85b8cb6afa3068
김산해 <sanhae1996@gmail.com>
2023-10-30 22:19:27 +0900

    Versioning REST API - URL Versioning

---
+++ src/main/java/com/rest/webservices/restfulwebservice/versioning/Name.java
@@ -0,0 +1,27 @@
+package com.rest.webservices.restfulwebservice.versioning;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV2{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/rest/webservices/restfulwebservice/versioning/PersonV2.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.restfulwebservice.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
